1e66ed32a6b02a06259d0dbb5fee88835236247e
bsdbx <rakbogdan56@gmail.com>
2026-09-07 12:03:03 +0300

    hyprland automatic workspaces

--- hypr/.config/hypr/hyprland.lua
+++ hypr/.config/hypr/hyprland.lua
@@ -6,9 +6,19 @@ hl.config({
 
 local mod = "SUPER"
 
+-- Application Binds
 hl.bind(mod .. " + Q", hl.dsp.exec_cmd("kitty"))
+
+-- Native Binds
 hl.bind(mod .. " + C", hl.dsp.window.kill())
 hl.bind(mod .. " + M", hl.dsp.exit())
 
+-- Fullscren Binds
 hl.bind(mod .. " + F", hl.dsp.window.fullscreen({ mode = 1 }))
 hl.bind(mod .. " + SHIFT + F", hl.dsp.window.fullscreen({ mode = 0 }))
+
+-- Workspace Binds
+for i = 1, 9 do
+    hl.bind(mod .. " + " .. i, hl.dsp.focus({ workspace = i }))
+    hl.bind(mod .. " + SHIFT + " .. i, hl.dsp.window.move({ workspace = i, follow = true }))
+end
